975d2f6c60d5c84cf6d26538e844f931060e207d
alexisreiner <152926007+alexisreiner@users.noreply.github.com>
2026-01-22 11:47:45 -0800

    Add username validation script

---
+++ username.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Username validation script
+
+regex='^[a-z][a-z0-9_]{2,11}$'
+
+echo "Username Rules:"
+echo "- Only lowercase letters, digits, and underscores allowed"
+echo "- Must start with a lowercase letter"
+echo "- Must be between 3 and 12 characters long"
+echo ""
+
+while true; do
+    echo -n "Enter a username: "
+    read username
+
+    if [[ $username =~ $regex ]]; then
+        echo "Valid username. Thank you!"
+        break
+    else
+        echo "Invalid username. Please try again."
+        echo ""
+    fi
+done
+
+
+
+
